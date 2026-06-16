3c884c9cdd463a29be5b3d0aad8e617b6e1293ec
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 16:12:53 +0530

    Send JWT to Client:

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const cookieParser=require('cookie-parser');
 app.use(cookieParser());
 const jwt=require('jsonwebtoken');
 
+// Sending JWT to Client:
 app.get('/',(req,res)=>{
-    res.cookie("name","pratham guruji");
-    res.send('done');
+    let tokenn = jwt.sign({email:"pratham@gmail.com"},"secret");
+    res.cookie("token",tokenn);
+    res.send("donee");
+    console.log(tokenn);
+
 })
+
+// set up the cookie
+// app.get('/',(req,res)=>{
+//     res.cookie("name","pratham guruji");
+//     res.send('done');
+// })
 
 app.listen(3000,()=>{
     console.log("running");
